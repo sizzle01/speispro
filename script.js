6cdc9759a527b64b796887645ab268d918cfedab
murtala Aliyu <talktoaliyu@gmail.com>
2020-10-08 16:07:43 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//onload function
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display");
+    }, );
+};
+//MENU FUNCTION
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container, .about-cont").classList.toggle("change");
+});
+//SCROLL UP FUNCTION
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+});
