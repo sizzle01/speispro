8ddcc2b899b798889b717f57abdf34c7e1a6d9d3
murtala Aliyu <talktoaliyu@gmail.com>
2020-10-14 15:32:00 +0100

    email link

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ window.onload = () => {
         document.querySelector("body").classList.add("display");
     }, );
 };
+
+
 //MENU FUNCTION
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container, .about-cont").classList.toggle("change");
 });
+
+
 //SCROLL UP FUNCTION
 document.querySelector(".scroll-btn").addEventListener("click", () => {
     document.querySelector("html").style.scrollBehavior = "smooth";
